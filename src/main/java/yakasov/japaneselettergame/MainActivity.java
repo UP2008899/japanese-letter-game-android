39230b58934bce291654f9b701a2fb5b22d9f97f
UP2008899 <UP2008899@users.noreply.github.com>
2022-02-18 17:31:00 +0000

    Add Settings Activity

--- src/main/java/yakasov/japaneselettergame/MainActivity.java
+++ src/main/java/yakasov/japaneselettergame/MainActivity.java
@@ -2,6 +2,7 @@ package yakasov.japaneselettergame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -127,5 +128,10 @@ public class MainActivity extends AppCompatActivity {
             score -= 50;
         }
         setAllCharacters();
+    }
+
+    public void settingsPressed(View view) {
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
